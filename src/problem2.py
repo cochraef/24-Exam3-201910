6d4d0f47009a4c911c0c85d2e471ae5c1e6da8bf
cochraef <cochraef@rose-hulman.edu>
2018-10-25 21:24:51 -0400

    problem 2 done

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Evan Cochrane.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # 1: DONE PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,9 +90,16 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # 2: DONE Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    largest = 0
+    for k in range(len(sequence)):
+        if abs(sequence[k]) > largest:
+            largest = abs(sequence[k])
+            value = k
+    return value
 
 
 ###############################################################################
